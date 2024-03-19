92b3f03b73ce89cda309d0ae6413f6ea985d06d5
dcrisanto <dorelly.crisanto@gmail.com>
2024-03-19 15:01:40 -0500

    Variables

---
+++ HolaMundo/src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Declarando una variable
+        int speed;
+        //Inicializando variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Dorelly del Rosario";
+        System.out.println(employeeName);
+        String employeeLastName = "Crisanto Silupú";
+        System.out.println(employeeLastName);
+        System.out.println(employeeName + " " + employeeLastName);
+    }
+}
