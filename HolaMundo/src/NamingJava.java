bd34f05701597a055e10fa174fe6f460f1ccf680
dcrisanto <dorelly.crisanto@gmail.com>
2024-03-19 16:09:26 -0500

    NamingJava

---
+++ HolaMundo/src/NamingJava.java
@@ -0,0 +1,16 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone = 984816063;
+        int celPhone = 985210270;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+        String $countryName = "Perú";
+        String _backgroundColor = "Green";
+        String currency$ = "MXN";
+        String background_color = "red";
+        //Constantes
+        int POSITION = -5;
+        int MAX_WIDTH = 999;
+        int MIN_WIDTH = 1;
+    }
+}
